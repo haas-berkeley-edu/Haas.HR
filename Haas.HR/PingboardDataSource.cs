8d79c219e9f8ddc3417dfd2abd388db07c4a02ff
pierrechew <pchew@BERKELEY.EDU>
2023-10-09 10:44:34 -0700

    Added missing comments.

--- Haas.HR/PingboardDataSource.cs
+++ Haas.HR/PingboardDataSource.cs
@@ -10,7 +10,9 @@ namespace Haas.HR
     /// Impelemntation of the Haas Pingboard instance.  This class is responsible for handling the downloading of the data in that instance
     /// into the Pingboard table in the HR Data Wharehouse.  It is also responsible for updating the Master employee record in the master employee
     /// table in the Data Wharehouse with the fields in Pingboard that are the master fields (ie. Reports To, Position Title).  Finally this class
-    /// is responsible for uploading data from the Master employee record in the HR Data Wharehouse that are referenced in Pingboard (ie. Seat, Phone Number)
+    /// is responsible for uploading data from the Master employee record in the HR Data Wharehouse that are referenced in Pingboard (ie. Seat, Phone Number).
+    /// The GetEmployeeProfile url method takes in the uid of the employee in question and returns the url that you would use to navigate to that employee
+    /// record in Pingboard.
     /// </summary>
     public class PingboardDataSource : HRDataSourceBase
     {
