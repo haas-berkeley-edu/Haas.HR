c256ad1d19e8a27909c18730f14e8a728d350300
pierrechew <pchew@BERKELEY.EDU>
2023-10-11 09:57:38 -0700

    Added entity framework

---
+++ Haas.HR/Data/HRDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Haas.HR.Models;
+
+namespace Haas.HR.Data
+{
+    public class HRDbContext : DbContext
+    {
+        public HRDbContext(DbContextOptions<HRDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PingboardEmployee> PingboardEmployees { get; set; }
+        public DbSet<UCPathEmployee> UCPathEmployees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PingboardEmployee>().ToTable("PingboardEmployee");
+            modelBuilder.Entity<UCPathEmployee>().ToTable("UCPathEmployee");
+        }
+    }
+}
---
+++ Haas.HR/Models/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haas.HR.Models
+{
+    public interface IEmployee
+    {
+        int ID { get; set; }
+    }
+}
